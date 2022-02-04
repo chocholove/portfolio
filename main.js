4b7cfccde79db89b6e850f6fa8da4293e2148aec
chocholove <gmerit0116@naver.com>
2022-02-04 13:26:49 +0900

    jS_Navbar menu toggle * Project UI tweak

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -24,9 +26,16 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
-    // console.log(event.target.dataset.link);
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
-});
+  });
+  
+  // Navbar toggle button for small screen
+  const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+  navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+  });
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
